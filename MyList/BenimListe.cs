5f8e197b00499e5ecfb0f61b46d5f50e53130493
kuzey <kuzey@DESKTOP-EVF6M2N>
2021-02-03 00:08:34 +0300

    Add project files.

---
+++ MyList/BenimListe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyList
+{
+    class BenimListe<Type>
+    {
+        // Type[] eleman=new Type[] { };
+        Type[] oyunAdi;
+        public BenimListe()
+        {
+            oyunAdi = new Type[0];
+        }
+        public void Add(Type type)
+        {
+            Type[] geciciDizi = oyunAdi;
+            oyunAdi = new Type[oyunAdi.Length+1];
+            for (int i = 0; i < geciciDizi.Length; i++)
+            {
+                oyunAdi[i] = geciciDizi[i];
+            }
+            oyunAdi[oyunAdi.Length - 1] = type;
+        }
+    }
+}
---
+++ MyList/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenimListe< string> hangiOyun = new BenimListe< string>();
+            hangiOyun.Add("Medal Of Honor"); // Formlarda TextBox dan yazdırabiliriz, yada Steam gibi bir formdan comboboxdan sectirebiliriz.
+            
+        }
+    }
+}
